dfcc0993a00cc04ac7cdd79cf78b2086a5e5794d
Virginiep4 <virginie.payet04@gmail.com>
2025-01-03 17:39:10 +0100

    version virginie

---
+++ src/entities/Entities.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public abstract class Entities {
+	protected static int posX = 0;
+	protected static int posY = 0;
+	
+	public Entities() {
+		importEntity();
+	}
+
+	
+	public static int getPosX() {
+		return posX;
+	}
+	public static int getPosY() {
+		return posY;
+	}
+	
+	/**
+	 * Puts all the frame of character animation in each character array
+	 */
+	abstract void importEntity();
+
+	/**
+	 * Load an image on stream and return it if is gotten
+	 *
+	 * @param path is the path of the image to load
+	 */
+	protected BufferedImage importImg(String path) {
+		InputStream is = getClass().getResourceAsStream(path);
+
+		try {
+			BufferedImage img = ImageIO.read(is);
+			is.close();
+			return img;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * Where the animation of the character are handled
+	 */
+	abstract void updateAnimationTick();
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module DreamTrap {
+	requires java.desktop;
+	requires java.sql;
+	requires mysql.connector.java;
+}
